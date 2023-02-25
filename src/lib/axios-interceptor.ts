ecbac7734f7248eed032dccdfc928c27860506e3
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2023-02-25 21:25:03 +0100

    Add Logout Module

--- src/lib/axios-interceptor.ts
+++ src/lib/axios-interceptor.ts
@@ -7,6 +7,20 @@ const TIMEOUT = 1 * 60 * 1000;
 axios.defaults.timeout = TIMEOUT;
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_END_POINT; // process.env.REACT_APP_API_END_POINT;
 
+let checkOneCall = true;
+function refreshToken() {
+  const config: any = {
+    url: "/api/user/public/refreshtoken",
+    method: "POST",
+    data: {
+      refreshToken: StorageService.local.get(
+        AllAppConfig.NAME_REFRESH_TOKEN_CURRENT_USER
+      )
+    }
+  };
+  return axios.request(config);
+}
+
 const setupAxiosInterceptors = (onUnauthenticated: () => void) => {
   const onRequestSuccess = (config: any) => {
     // config.headers.localTest = 'ar';
@@ -32,9 +46,39 @@ const setupAxiosInterceptors = (onUnauthenticated: () => void) => {
   };
   const onResponseError = (err: any) => {
     const status = err.status || (err.response ? err.response.status : 0);
-    if (status === 403 || status === 401) {
-      onUnauthenticated();
+
+    if (status === 401) {
+      if (checkOneCall) {
+        checkOneCall = false;
+        refreshToken().then(
+          (resultRefreshToken) => {
+            if (resultRefreshToken) {
+              StorageService.local.set(
+                AllAppConfig.NAME_TOKEN_CURRENT_USER,
+                resultRefreshToken?.data?.token
+              );
+              checkOneCall = true;
+              return Promise.resolve(resultRefreshToken);
+            } else {
+              onUnauthenticated();
+              return Promise.reject(resultRefreshToken);
+            }
+          },
+          (errorRefreshToken) => {
+            onUnauthenticated();
+            return Promise.reject(errorRefreshToken);
+          }
+        );
+      } else {
+        return Promise.reject(err);
+      }
     }
+
+    if (status === 403) {
+      onUnauthenticated();
+      return Promise.reject(err);
+    }
+
     return Promise.reject(err);
   };
   axios.interceptors.request.use(onRequestSuccess, onRequestError);
